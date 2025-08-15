ab3ec33a0019590665f011c4fe49f43ad1c46d74
y3u1 <228891677@qq.com>
2025-08-15 21:58:54 +0800

    add Test

--- src/test/java/com/jie/de/controller/TeacherControllerTest.java
+++ src/test/java/com/jie/de/controller/TeacherControllerTest.java
@@ -4,42 +4,50 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@WithMockUser(username = "yue", roles = {"STUDENT"})
-class StudentControllerTest {
+class TeacherControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-
+    @Transactional
+    @Rollback()
     @Test
-    public void testGetExitUserInfo() throws Exception {
-        Long userId = 202205566215L;
-        mockMvc.perform(MockMvcRequestBuilders
-                .get("/student/studentInfo/" + userId)
+    public void testRegisterStudent() throws Exception {
+        String json = "{ \"username\": \"jie\", \"password\": \"qpalzm1234\", \"userId\": \"202205566299\" }";
+        mockMvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+                .post("/teacher/studentRegister")
+                .content(json.getBytes())
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
         )
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
                 .andDo(print());
     }
+    @Transactional
+    @Rollback()
     @Test
-    public void testGetUnExistsUserInfo() throws Exception {
-        Long userId = 1234L;
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/student/studentInfo/" + userId)
+    public void testRegisterWithRepeteUseridStudent() throws Exception {
+        String json = "{ \"username\": \"jie\", \"password\": \"qpalzm1234\", \"userId\": \"202205566215\" }";
+        mockMvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+                        .post("/teacher/studentRegister")
+                        .content(json.getBytes())
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                 )
                 .andExpect(MockMvcResultMatchers.status().is4xxClientError())
                 .andDo(print());
     }
+
+
 }
--- src/test/java/com/jie/de/controller/commoeControllerTest.java
+++ src/test/java/com/jie/de/controller/commoeControllerTest.java
@@ -1,37 +1,64 @@
 package com.jie.de.controller;
 
-import com.jie.de.model.dto.LoginDTO;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+
+import org.springframework.http.MediaType;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class commoeControllerTest {
-    @Autowired
-    private TestRestTemplate TestRestTemplate;
-    LoginDTO ldto = new LoginDTO()
-    {{
-        setUsername("yue");
-        setPassword("123456");
-    }};
 
-    LoginDTO wldto = new LoginDTO()
-    {{
-        setUsername("yue");
-        setPassword("1234567");
-    }};
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    public void testExistUser() {
-        ResponseEntity<String> response = TestRestTemplate.postForEntity("/user/login", ldto, String.class);
-        assert(response.getStatusCode().is2xxSuccessful());
+    public void testExistUser() throws Exception {
+        String json = "{ \"username\": \"yue\", \"password\": \"123456\" }";
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/user/login")
+                .content(json.getBytes())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(print());
     }
     @Test
-    public void testNotExistUser() {
-        ResponseEntity<String> response = TestRestTemplate.postForEntity("/user/login", wldto, String.class);
-        assert(response.getStatusCode().is4xxClientError());
+    public void testNotExistUser() throws Exception {
+        String json = "{ \"username\": \"hhh\", \"password\": \"123456\" }";
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/user/login")
+                        .content(json.getBytes())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError())
+                .andDo(print());
+    }
+    @Test
+    public void testWithEmptyPassword() throws Exception {
+        String json = "{ \"username\": \"yue\", \"password\": \"\" }";
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/user/login")
+                        .content(json.getBytes())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError())
+                .andDo(print());
     }
 
 
